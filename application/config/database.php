954c98e134a43eb69196874ee9de50326aa404e9
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-11-10 15:29:00 +0700

    db hosting

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '151.106.119.126',
+	'username' => 'korpora2018_resource_therapy',
+	'password' => 'resource_therapy',
+	'database' => 'korpora2018_resource_therapy',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
