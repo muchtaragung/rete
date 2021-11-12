f7de09503ab2b172a116e0a77e5a9bbb6b83811d
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-11-12 14:05:14 +0700

    update artikel lewat adminn

--- application/views/admin/layouts/head.php
+++ application/views/admin/layouts/head.php
@@ -10,3 +10,8 @@
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 <!-- Theme style -->
 <link rel="stylesheet" href="<?= base_url('assets/admin/') ?>dist/css/adminlte.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/admin/') ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/admin/') ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/admin/') ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<!-- summernote -->
+<link rel="stylesheet" href="<?= base_url('assets/admin/') ?>plugins/summernote/summernote-bs4.min.css">
