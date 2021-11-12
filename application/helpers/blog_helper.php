16f09f7a40858c14f5ff0fedd635e50567ea05bf
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-11-12 15:09:09 +0700

    creating helper limit echo

---
+++ application/helpers/blog_helper.php
@@ -0,0 +1,10 @@
+<?php
+function limit_echo($x, $length)
+{
+    if (strlen($x) <= $length) {
+        echo $x;
+    } else {
+        $y = substr($x, 0, $length) . '...';
+        echo $y;
+    }
+}
